d4885aee3270de7e0e83e2a0154f1129a2ab68bd
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-11-12 17:44:56 +0100

    Add Kafka spout

---
+++ consumer/src/main/java/aic/g3t1/consumer/spout/AicKafkaSpout.java
@@ -0,0 +1,34 @@
+package aic.g3t1.consumer.spout;
+
+import aic.g3t1.common.environment.EnvironmentVariables;
+import aic.g3t1.common.exceptions.MissingEnvironmentVariableException;
+import aic.g3t1.common.taxiposition.TaxiPosition;
+import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.kafka.spout.KafkaSpoutConfig;
+
+import java.util.Properties;
+
+public class AicKafkaSpout extends KafkaSpout<String, TaxiPosition> {
+
+    public static final String KAFKA_TOPIC = "taxi";
+
+    public AicKafkaSpout() throws MissingEnvironmentVariableException {
+        super(getKafkaSpoutConfig());
+    }
+
+    public static KafkaSpoutConfig<String, TaxiPosition> getKafkaSpoutConfig()
+            throws MissingEnvironmentVariableException {
+        String bootstrapServer = EnvironmentVariables.getVariable("KAFKA_BOOTSTRAP_SERVER");
+
+        var properties = new Properties();
+        properties.setProperty("group.id", EnvironmentVariables.getVariable("KAFKA_GROUP_ID"));
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "aic.g3t1.common.taxiposition.TaxiPositionDeserializer");
+
+        var builder = new KafkaSpoutConfig.Builder<String, TaxiPosition>(bootstrapServer, KAFKA_TOPIC);
+        builder.setProcessingGuarantee(KafkaSpoutConfig.ProcessingGuarantee.AT_LEAST_ONCE);
+        builder.setProp(properties);
+        return new KafkaSpoutConfig<>(builder);
+    }
+
+}
