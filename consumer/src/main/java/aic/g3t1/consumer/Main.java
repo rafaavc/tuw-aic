5e0ee701b1ac01e4c82c52e241f7686dc17696fa
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-11-12 17:47:04 +0100

    Add main Storm submission configuration

--- consumer/src/main/java/aic/g3t1/consumer/Main.java
+++ consumer/src/main/java/aic/g3t1/consumer/Main.java
@@ -1,22 +1,20 @@
 package aic.g3t1.consumer;
 
-import aic.g3t1.common.taxiposition.TaxiPosition;
-import aic.g3t1.common.kafka.ConsumerFactory;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-
-import java.time.Duration;
-import java.util.List;
+import aic.g3t1.consumer.topology.AicTopology;
+import org.apache.storm.Config;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        var consumer = ConsumerFactory.createConsumer();
 
-        consumer.subscribe(List.of("taxi"));
-        while (true) {
-            ConsumerRecords<String, TaxiPosition> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, TaxiPosition> record : records)
-                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value().toString());
-        }
+    public static void main(String[] args) throws Exception {
+        AicTopology topology = new AicTopology();
+        topology.submit(getConfig());
     }
+
+    private static Config getConfig() {
+        Config config = new Config();
+        config.setDebug(true);
+        config.setNumWorkers(3);
+        return config;
+    }
+
 }
