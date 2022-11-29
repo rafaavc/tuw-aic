c4121c101290458aa4f7c73498369b05c4ccc2fe
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-11-29 15:18:26 +0100

    Add KafkaTupleBolt

---
+++ consumer/src/main/java/aic/g3t1/consumer/bolt/KafkaTupleBolt.java
@@ -0,0 +1,35 @@
+package aic.g3t1.consumer.bolt;
+
+import aic.g3t1.common.taxiposition.TaxiPosition;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.List;
+import java.util.Map;
+
+public class KafkaTupleBolt extends BaseRichBolt {
+
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map<String, Object> topoConf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        TaxiPosition position = (TaxiPosition) input.getValueByField("value");
+        collector.emit(input, List.of(position.getTaxiNumber(), position.getTimestamp(), position.getLongitude(), position.getLatitude()));
+        collector.ack(input);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("taxiNumber", "timestamp", "longitude", "latitude"));
+    }
+
+}
