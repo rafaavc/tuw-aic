be6850aff32de56c69df6d1a49952e69dee8468e
Xavier Pisco <x.pisco@gmail.com>
2022-11-29 21:32:13 +0100

    Fixed producer speed, now sends messages correctly if there are no messages for some timestamps

--- producer/src/main/java/aic/g3t1/producer/TaxiPositionSender.java
+++ producer/src/main/java/aic/g3t1/producer/TaxiPositionSender.java
@@ -4,12 +4,14 @@ import aic.g3t1.common.taxiposition.TaxiPosition;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class TaxiPositionSender implements Runnable {
     private final List<TaxiPosition> taxiPositions;
     private static int positionsSent = 0;
+    private static Date nextTimestamp;
     private final ScheduledExecutorService executor;
     private final KafkaProducer<String, TaxiPosition> producer;
     private final String topic;
@@ -19,21 +21,22 @@ public class TaxiPositionSender implements Runnable {
         this.executor = executor;
         this.producer = producer;
         this.topic = topic;
+        nextTimestamp = taxiPositions.get(0).getTimestamp();
     }
 
     @Override
     public void run() {
-        var firstPositionToSend = taxiPositions.get(positionsSent);
         while (positionsSent < taxiPositions.size()) {
             var nextPosition = taxiPositions.get(positionsSent);
-            if (nextPosition.getTimestamp().after(firstPositionToSend.getTimestamp())) {
+            if (nextPosition.getTimestamp().after(nextTimestamp)) {
+                nextTimestamp = new Date(nextTimestamp.getTime() + 1000);
                 break;
             }
             var record = new ProducerRecord<>(topic, String.valueOf(positionsSent), nextPosition);
             producer.send(record);
             positionsSent++;
         }
-        System.out.println("Sent positions with timestamp = " + firstPositionToSend.getTimestamp());
+        System.out.println("Sent positions with timestamp = " + nextTimestamp);
 
         if (positionsSent >= taxiPositions.size()) {
             System.out.println("All available taxi positions were sent, closing.");
