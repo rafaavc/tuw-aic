08eaf55c5e1043673c6b981f95d2f1345ff91b49
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-12-03 21:48:34 +0100

    Add CalculateDistanceBolt to topology

--- consumer/src/main/java/aic/g3t1/consumer/topology/AicTopology.java
+++ consumer/src/main/java/aic/g3t1/consumer/topology/AicTopology.java
@@ -1,6 +1,7 @@
 package aic.g3t1.consumer.topology;
 
 import aic.g3t1.common.exceptions.MissingEnvironmentVariableException;
+import aic.g3t1.consumer.bolt.CalculateDistanceBolt;
 import aic.g3t1.consumer.bolt.KafkaTupleBolt;
 import aic.g3t1.consumer.sink.DebugSink;
 import aic.g3t1.consumer.spout.AicKafkaSpout;
@@ -13,7 +14,7 @@ import org.apache.storm.redis.common.config.JedisPoolConfig;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 
-import static aic.g3t1.consumer.spout.TaxiPositionFields.*;
+import static aic.g3t1.consumer.spout.TaxiPositionFields.F_TAXI_NUMBER;
 
 public class AicTopology {
 
@@ -23,6 +24,7 @@ public class AicTopology {
     private static final String TOPOLOGY_NAME = "aic-topology";
     private static final String KAFKA_SPOUT_ID = "kafka_spout";
     private static final String KAFKA_TUPLE_BOLT_ID = "kafka_tuple_bolt";
+    private static final String CALCULATE_DISTANCE_BOLT_ID = "calculate_distance_bolt";
     private static final String DEBUG_SINK_ID = "debug_sink";
 
     private final TopologyBuilder BUILDER = new TopologyBuilder();
@@ -40,6 +42,9 @@ public class AicTopology {
         BUILDER.setSpout(KAFKA_SPOUT_ID, new AicKafkaSpout(), 1);
         BUILDER.setBolt(KAFKA_TUPLE_BOLT_ID, new KafkaTupleBolt(), 1).globalGrouping(KAFKA_SPOUT_ID);
         BUILDER.setBolt(DEBUG_SINK_ID, new DebugSink()).fieldsGrouping(KAFKA_TUPLE_BOLT_ID, new Fields(F_TAXI_NUMBER));
+
+        BUILDER.setBolt(CALCULATE_DISTANCE_BOLT_ID, new CalculateDistanceBolt(jedisPoolConfig))
+                .fieldsGrouping(KAFKA_TUPLE_BOLT_ID, new Fields(F_TAXI_NUMBER));
     }
 
     public void submit(Config config) throws AuthorizationException, InvalidTopologyException, AlreadyAliveException {
