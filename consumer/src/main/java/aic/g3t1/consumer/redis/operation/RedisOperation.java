0aa201d4301fe33bbcf6b6f979311197cda33da0
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-12-03 21:28:21 +0100

    Add RedisOperation interface

---
+++ consumer/src/main/java/aic/g3t1/consumer/redis/operation/RedisOperation.java
@@ -0,0 +1,13 @@
+package aic.g3t1.consumer.redis.operation;
+
+import redis.clients.jedis.JedisCommands;
+
+import java.io.Serializable;
+
+public interface RedisOperation extends Serializable {
+
+    String F_REDIS_OPERATION = "redis_op";
+
+    void perform(JedisCommands commands);
+
+}
