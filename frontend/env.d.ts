57a5436b4836105ab3010f5ecf072cefd8fd0f04
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-12-23 22:55:32 +0100

    Add Vite ImportMetaEnv type definitions

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_HOST: string | undefined
+  readonly VITE_BACKEND_PROTOCOL_WS: string | undefined
+  readonly VITE_BACKEND_PORT_WS: string | undefined
+  readonly VITE_BACKEND_PATH_WS: string | undefined
+  readonly VITE_WS_TOPIC_DRIVING_TAXIS: string | undefined
+  readonly VITE_WS_TOPIC_TAXI_DISTANCES: string | undefined
+  readonly VITE_WS_TOPIC_SPEEDING_INCIDENTS: string | undefined
+  readonly VITE_WS_TOPIC_AREA_VIOLATIONS: string | undefined
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
