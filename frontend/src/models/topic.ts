432808d5075c17532cd844fdf16d026fca658597
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-12-25 21:00:25 +0100

    Add TopicDataMapping

--- frontend/src/models/topic.ts
+++ frontend/src/models/topic.ts
@@ -1,3 +1,7 @@
+import type { TaxiDistances } from "@/models/taxi-distances";
+import type { AreaViolation } from "@/models/area-violation";
+import type { SpeedingIncident } from "@/models/speeding-incident";
+
 export enum Topic {
   DRIVING_TAXIS,
   TAXI_DISTANCES,
@@ -6,3 +10,12 @@ export enum Topic {
 }
 
 export type TopicEventMapping = Record<Topic, string>
+
+type TopicDataMappingBase = Record<Topic, any>
+
+export interface TopicDataMapping extends TopicDataMappingBase {
+  [Topic.DRIVING_TAXIS]: any
+  [Topic.TAXI_DISTANCES]: TaxiDistances
+  [Topic.AREA_VIOLATIONS]: AreaViolation
+  [Topic.SPEEDING_INCIDENTS]: SpeedingIncident
+}
