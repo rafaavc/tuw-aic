f4b01d5235f3dcbcecee5a24378f7e01ba64243d
Andre Mategka <e11809614@student.tuwien.ac.at>
2022-12-30 21:57:26 +0100

    Fix duplicate entries in notification lists

--- frontend/src/components/Statistics.vue
+++ frontend/src/components/Statistics.vue
@@ -3,12 +3,13 @@ import { WebsocketClient } from "@/services/websocket-client";
 import { type TaxiNotification, type TaxiNumber, Topic } from "@/models";
 import { computed, reactive, ref } from "vue";
 import { rounded } from "@/services/numbers";
+import { pushToValue } from "@/services/maps";
 
 const drivingTaxiCount = ref<number>(0)
 const totalDistance = ref<number>(0)
 const totalDistanceRounded = computed(() => rounded(totalDistance.value, 2))
-const areaViolations = reactive<TaxiNotification[]>([])
-const speedingIncidents = reactive<TaxiNotification[]>([])
+const areaViolations = reactive<Map<TaxiNumber, TaxiNotification[]>>(new Map())
+const speedingIncidents = reactive<Map<TaxiNumber, TaxiNotification[]>>(new Map())
 
 const socket = new WebsocketClient()
 socket.connect().then(() => {
@@ -17,10 +18,12 @@ socket.connect().then(() => {
     totalDistance.value = data.totalDistance
   })
   socket.on(Topic.LEAVING_AREA).subscribe((violation) => {
-    areaViolations.push(violation)
+    const { taxiNumber } = violation;
+    pushToValue(areaViolations, taxiNumber, violation)
   })
   socket.on(Topic.SPEEDING).subscribe((incident) => {
-    speedingIncidents.push(incident)
+    const { taxiNumber } = incident;
+    pushToValue(speedingIncidents, taxiNumber, incident)
   })
 })
 </script>
@@ -35,9 +38,12 @@ socket.connect().then(() => {
     </div>
     <div class="col-12 col-md-4">
       <p class="h4">Area violations</p>
-      <ul v-if="areaViolations.length > 0">
-        <li v-for="areaViolation of areaViolations">
-          Taxi {{ areaViolation.taxiNumber }}
+      <ul v-if="areaViolations.size > 0">
+        <li v-for="[taxiNumber, violations] of areaViolations.entries()">
+          Taxi {{ taxiNumber }}
+          <span v-if="violations.length > 1">
+            ({{ violations.length }}x)
+          </span>
         </li>
       </ul>
       <p v-else>
@@ -46,9 +52,12 @@ socket.connect().then(() => {
     </div>
     <div class="col-12 col-md-4">
       <p class="h4">Speeding incidents</p>
-      <ul v-if="speedingIncidents.length > 0">
-        <li v-for="speedingIncident of speedingIncidents">
-          Taxi {{ speedingIncident.taxiNumber }}
+      <ul v-if="speedingIncidents.size > 0">
+        <li v-for="[taxiNumber, incidents] of speedingIncidents.entries()">
+          Taxi {{ taxiNumber }}
+          <span v-if="incidents.length > 1">
+            ({{ incidents.length }}x)
+          </span>
         </li>
       </ul>
       <p v-else>
