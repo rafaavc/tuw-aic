b595cd7dbc2a74ecc77292aa95c1ae1732f458aa
Andre Mategka <e11809614@student.tuwien.ac.at>
2023-01-03 16:38:04 +0100

    Make supervisor wait for zookeeper

--- storm-supervisor/start.sh
+++ storm-supervisor/start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "[WAIT] Waiting for Zookeeper... ($WAIT_ZOOKEEPER)"
+/wait-for "$WAIT_ZOOKEEPER" -t 60 || exit 100
+echo "[WAIT] Zookeeper connected!"
+
 echo "[WAIT] Waiting for nimbus... ($WAIT_NIMBUS)"
 /wait-for "$WAIT_NIMBUS" -t 60 || exit 104
 echo "[WAIT] Nimbus connected!"
+
 sleep 1s
 echo "Finishing storm container setup..."
 /docker-entrypoint.sh || exit 111
