584064a02e83231b4d6997c5d601a1c5456ffb31
Andre Mategka <e11809614@student.tuwien.ac.at>
2023-01-06 00:44:55 +0100

    Implement overflow for notifications

--- frontend/src/components/Statistics.vue
+++ frontend/src/components/Statistics.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { WebsocketClient } from "@/services/websocket-client"
 import { type TaxiNotification, type TaxiNumber, Topic } from "@/models"
-import { computed, reactive, Ref, ref } from "vue"
+import { computed, reactive, ref } from "vue"
 import { rounded } from "@/services/numbers"
 import { pushToValue } from "@/services/maps"
+
+type NotificationMap = Map<TaxiNumber, TaxiNotification[]>
+
+const DISPLAY_LIMIT = 4
 
 const drivingTaxiCount = ref<number>(0)
 const totalDistance = ref<number>(0)
 const totalDistanceRounded = computed(() => rounded(totalDistance.value, 2))
-const areaViolations = reactive<Map<TaxiNumber, TaxiNotification[]>>(new Map())
-const speedingIncidents = reactive<Map<TaxiNumber, TaxiNotification[]>>(new Map())
+const areaViolations = reactive<NotificationMap>(new Map())
+const speedingIncidents = reactive<NotificationMap>(new Map())
+const showAllAreaViolations = ref(false)
+const showAllSpeedingIncidents = ref(false)
+const avLimit = computed(() => showAllAreaViolations.value ? -1 : DISPLAY_LIMIT)
+const siLimit = computed(() => showAllSpeedingIncidents.value ? -1 : DISPLAY_LIMIT)
+const areaViolationsDisplayed = computed(() => sortByTimestamps(areaViolations, avLimit.value))
+const speedingIncidentsDisplayed = computed(() => sortByTimestamps(areaViolations, siLimit.value))
 
 const dateFormatter = new Intl.DateTimeFormat('en-GB', { dateStyle: 'long', timeStyle: 'medium' } as any)
 
@@ -21,9 +31,10 @@ function last<T>(array: T[]): T {
   return array[array.length - 1]
 }
 
-function sortByTimestamps(map: Map<TaxiNumber, TaxiNotification[]>): Array<[TaxiNumber, TaxiNotification[]]> {
+function sortByTimestamps(map: NotificationMap, limit: number): Array<[TaxiNumber, TaxiNotification[]]> {
   const array = [...map.entries()]
-  return array.sort(([_1, a], [_2, b]) => last(b).timestamp.getTime() - last(a).timestamp.getTime())
+  array.sort(([_1, a], [_2, b]) => last(b).timestamp.getTime() - last(a).timestamp.getTime())
+  return (limit > 0) ? array.slice(0, limit) : array
 }
 
 const socket = new WebsocketClient()
@@ -62,7 +73,7 @@ socket.connect().then(() => {
     <div class="col-12 col-md-4">
       <p class="h4">Area violations</p>
       <ul v-if="areaViolations.size > 0" class="list-group">
-        <li v-for="[taxiNumber, violations] of sortByTimestamps(areaViolations)"
+        <li v-for="[taxiNumber, violations] of areaViolationsDisplayed"
             class="list-group-item d-flex justify-content-between align-items-center">
           <span v-tooltip.right="formatDate(last(violations).timestamp)">
             Taxi {{ taxiNumber }}
@@ -75,6 +86,10 @@ socket.connect().then(() => {
             {{ violations.length }}
           </span>
         </li>
+        <button v-if="areaViolations.size > DISPLAY_LIMIT" class="list-group-item list-group-item-action"
+            @click="showAllAreaViolations = !showAllAreaViolations">
+          {{ showAllAreaViolations ? "Collapse" : "Expand" }}
+        </button>
       </ul>
       <div v-else class="card">
         <div class="card-body">
@@ -85,7 +100,7 @@ socket.connect().then(() => {
     <div class="col-12 col-md-4">
       <p class="h4">Speeding incidents</p>
       <ul v-if="speedingIncidents.size > 0" class="list-group">
-        <li v-for="[taxiNumber, incidents] of sortByTimestamps(speedingIncidents)"
+        <li v-for="[taxiNumber, incidents] of speedingIncidentsDisplayed"
             class="list-group-item d-flex justify-content-between align-items-center">
           <span v-tooltip.right="formatDate(last(incidents).timestamp)">
             Taxi {{ taxiNumber }}
@@ -98,6 +113,10 @@ socket.connect().then(() => {
             {{ incidents.length }}
           </span>
         </li>
+        <button v-if="speedingIncidents.size > DISPLAY_LIMIT" class="list-group-item list-group-item-action"
+            @click="showAllSpeedingIncidents = !showAllSpeedingIncidents">
+          {{ showAllSpeedingIncidents ? "Collapse" : "Expand" }}
+        </button>
       </ul>
       <div v-else class="card">
         <div class="card-body">
